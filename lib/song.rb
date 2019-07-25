1d85efe389bb967387ce860309d81a3fa388670f
Veronica Lopez <nicaa0695@gmail.com>
2019-07-25 19:41:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,11 +25,7 @@ class Song
     song 
   end 
   def self.find_by_name(name)
-    @@all.each do |song|
-      if song.name == name 
-        return song 
-      end
-    end 
+    @@all.find{|song| song.name == name}
   end
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
